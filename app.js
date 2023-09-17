b0c4ce52a2f4601ee0796180daafa563dd2966de
Dario <dario.sch@gmail.com>
2023-09-17 16:25:06 -0300

    finished styles and added light mode

--- app.js
+++ app.js
@@ -34,4 +34,11 @@ function PageTransitions() {
   });
 }
 
+//Toggle theme
+const themeBtn = document.querySelector(".theme-btn");
+themeBtn.addEventListener("click", () => {
+  let element = document.body;
+  element.classList.toggle("light-mode");
+});
+
 PageTransitions();
